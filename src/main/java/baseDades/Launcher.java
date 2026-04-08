74b4aba30c3ebb6de99cdae6dce50c6767c55fa2
cuests <sergi.cuesta2@gmail.com>
2026-04-08 10:09:29 +0200

    Update

---
+++ src/main/java/baseDades/Launcher.java
@@ -0,0 +1,8 @@
+package baseDades;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Esto "engaña" a Java para que cargue las librerías antes de arrancar la interfaz
+        Main.main(args); 
+    }
+}
--- src/main/java/baseDades/Main.java
+++ src/main/java/baseDades/Main.java
@@ -1,24 +1,44 @@
 package baseDades;
 
-import java.sql.SQLException;
-import java.util.Scanner;
-
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.h2.tools.Server;
-
 import baseDades.dao.DataBase.DatabaseInit;
 
-public class Main {
-    public static void main(String[] args) throws SQLException {
-        Server webServer = Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
-        System.out.println("El link es = " + webServer.getURL());
+// 1. Añadimos "extends Application" para decirle a Java que esto es una ventana de JavaFX
+public class Main extends Application {
+
+    // 2. Este método "start" es el que "dibuja" la ventana al arrancar
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Leemos tu archivo FXML (asegúrate de que está suelto en src/main/resources/)
+        Parent root = FXMLLoader.load(getClass().getResource("/event-form.fxml"));
         
+        // Creamos la "escena" (el contenido) con un tamaño inicial de 400x500 píxeles
+        Scene scene = new Scene(root, 400, 500);
+        
+        // Le ponemos un título a la ventana y la mostramos
+        primaryStage.setTitle("Buscador de Eventos");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-        DatabaseInit.init();
+    public static void main(String[] args) {
+        try {
+            // A. Iniciamos la base de datos H2 como ya hacíamos antes
+            Server webServer = Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
+            System.out.println("El link de la base de datos es = " + webServer.getURL());
+            DatabaseInit.init();
+            
+        } catch (Exception e) {
+            System.err.println("Error al iniciar la base de datos:");
+            e.printStackTrace();
+        }
 
-   
-        System.out.println("Presiona ENTER para apagar el servidor y salir...");
-        Scanner teclat = new Scanner(System.in);
-        teclat.nextLine();
-        teclat.close();
+        // B. Arrancamos la interfaz gráfica (esto llama al método start de arriba)
+        launch(args);
     }
 }
